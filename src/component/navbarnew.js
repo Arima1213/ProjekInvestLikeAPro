de998974d122a4af9fc74c776f0808eb9c2c585d
Arima1213 <arima3992@gmail.com>
2023-04-03 12:54:00 +0700

    perubahan resposif

--- src/component/navbarnew.js
+++ src/component/navbarnew.js
@@ -15,28 +15,19 @@ function NavBarKu() {
       fixed="top"
     >
       <Container>
-        <Navbar.Brand href="#">
+        <Navbar.Brand href="#hero">
           Invest Like a <span className="text-primary">Pro</span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
+          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }} navbarScroll>
             <Nav.Link href="#content1">Terkini</Nav.Link>
             <Nav.Link href="#content2">Trending</Nav.Link>
             <Nav.Link href="#content3">apps</Nav.Link>
             <Nav.Link href="#content4">Vidio</Nav.Link>
           </Nav>
           <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
+            <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
             <Button variant="outline-success">Search</Button>
           </Form>
         </Navbar.Collapse>
